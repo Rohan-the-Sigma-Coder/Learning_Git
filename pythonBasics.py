9230e540b222c409be3e8d79d6045ce296060887
Rohan Jha <rohan-jha@outlook.com>
2025-04-23 21:14:17 -0700

    final commit for 4/23/2025

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-import numpy as np
-xpoints = np.array([x for x in range(10)])
-ypoints = np.array([x for x in range(10)])
-plt.pie(xpoints)
-plt.show()
+import re
+
+txt = 'The rain in Spain'
+x = re.findall('\AThe', txt)
+print(x)
