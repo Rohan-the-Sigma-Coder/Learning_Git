cf1393a7c13fc3862507cf4215f8e45ca54b440c
Rohan Jha <rohan-jha@outlook.com>
2025-05-06 17:09:21 -0700

    modify penny_doubler

--- penny_doubler.py
+++ penny_doubler.py
@@ -1,8 +1,20 @@
-def doubler_penny(days):
+def doubler_penny_1(days):
     amount = 0.01
     for i in range(1, days + 1):
         print(f'Day: {i}: ${amount:.100f}')
         amount *= 2
 
 
-doubler_penny(200)
+
+def doubler_penny_2():
+    global output
+    output = [0.01]
+    for i in range(30):
+        output.append(output[len(output) - 1] * 2)
+    for x in range(0, len(output) - 1):
+        print(f'Day {x + 1}: {output[x]}')
+
+
+
+doubler_penny_2()
+
