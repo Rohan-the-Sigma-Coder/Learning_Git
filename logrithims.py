1443996522f7034f4a08c3f10f63a53ff0a8d291
Rohan Jha <rohan-jha@outlook.com>
2025-07-20 16:23:36 -0700

    Modify logrithims.py

--- logrithims.py
+++ logrithims.py
@@ -1,18 +1,26 @@
 import math
 
-argument = int(input("Enter argument: "))
-base = int(input("Enter base: "))
-bool = True
+perfect_log = True
 answer = 0
 
-while argument > 1:
-    if argument % base == 0:
-        argument = argument/base
+argument = int(input("Enter argument: "))
+base = int(input("Enter base: "))
+
+def logrithim_calculator(perfect_log, answer, argument, base):
+    while argument > 1:
+        if argument % base == 0:
+            argument = argument/base
+        else:
+            perfect_log = False
+            break
+        answer += 1
+
+
+    if perfect_log == True: 
+        return answer
     else:
-        bool = False
-        print('It is not perfect logrithim')
-        break
-    answer += 1
+        return "Not a perfect logrithim"
+    
 
 
-if bool == True: print(answer) 
+print(logrithim_calculator(perfect_log, answer, argument, base))
